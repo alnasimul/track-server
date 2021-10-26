089d7a4fbef43a40d4685ac5d0931267ffea4cf6
Nasimul Haque <alnasimul@gmail.com>
2021-10-26 23:48:02 +0600

    middleware added

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const authRoutes =  require('./routes/authRoutes');
+const requireAuth = require('./middlewares/requireAuth')
 require('dotenv').config();
 
 const app = express();
@@ -11,9 +12,7 @@ app.use(bodyParser.json());
 app.use(authRoutes);
 
 
-app.get('/', (req,res) => {
-    res.send('Hi there!')
-});
+
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.66naq.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
@@ -26,6 +25,10 @@ mongoose.connection.on('connected', () => {
 
 mongoose.connection.on('error', (err) => {
     console.error('Error connecting to mongo', err);
+});
+
+app.get('/', requireAuth, (req,res) => {
+    res.send(`Your email: ${req.user.email}`);
 });
 
 app.listen(4000, () => {
